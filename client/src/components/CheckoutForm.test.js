aecf526a7c5d1fedbbcaca1aac76a416b53ec098
cristian robles <gogo90553@icloud.com>
2020-07-19 21:39:40 -0500

    finished checkout form

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -4,6 +4,13 @@ import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    const { getByText } = render (<App/>);
+    const header = getByText(/React Plants/i);
+    expect(header).toBeInTheDocument();
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    const { getByText } = render (<useForm/>);
+    const message = getByText(/You have ordered some plants/i);
+});
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,34 @@
 // write your custom hook here to control your checkout form
+import { useState } from 'react';
+import { useLocalStorage } from 'local-storage';
+
+
+export const useForm = (initialState, key) => {
+    const [values, setvalues] = useLocalStorage (
+        initialState,
+        key
+
+    );
+
+    const [showSuccessMessage, setshowSuccessMessage] = useState(false);
+
+    const handleChanges = e => {
+        setvalues({
+            ...values,
+            [e.target.name]: e.target.value
+
+        });
+    };
+
+    const clearForm = () => {
+        setvalues(initialState)
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        clearForm()
+        setshowSuccessMessage(true)
+    };
+
+    return [ values, handleChanges, handleSubmit, showSuccessMessage ];
+};
